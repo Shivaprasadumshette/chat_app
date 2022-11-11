6f7eebef0b46c6955d55f2b808914a12728e7b3b
Shivaprasadumshette <2020BTEIT00042@walchandsangli.ac.in>
2022-11-11 11:11:46 +0530

    changess

---
+++ CHAT APP/js/client.js
@@ -0,0 +1,7 @@
+const socket = io('http://localhost:8000');
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+
+const j = prompt("Enter your name to join ");
+socket.emit('new-user-joined', j);
